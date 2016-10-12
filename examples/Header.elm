880bdcc68de3b50a4508bd75f67d90c6ae73e04c
trodrigu <trodriguez91@icloud.com>
2016-10-12 07:40:33 -0700

    update example for updated Animation lib and updated Scroll lib

--- examples/Header.elm
+++ examples/Header.elm
@@ -1,96 +1,114 @@
+port module Header exposing (..)
 import Scroll exposing (Move)
 import Html exposing (..)
+import Html.App as App
 import Html.Attributes exposing (..)
 import Animation as UI exposing (px, percent, width, height, backgroundColor)
-import Animation.Properties exposing (..)
 import Time exposing (second)
 import Platform.Cmd as Cmd exposing (Cmd)
+import Color exposing (rgb, rgba)
 
+main : Program Never
 main =
-    app.html
-
-
-app =
-    Html.program
+    App.program
         { init = init
         , view = view
         , update = update
         , subscriptions = subscriptions
         }
 
-subscriptions : Model -> Sub msg
+subscriptions : Model -> Sub Msg
 subscriptions model =
-  scroll Move
+    Sub.batch
+    [ UI.subscription Animate [model.style]
+    , scroll Header
+    ]
 
-
-port tasks : (Task Never ()) -> Cmd msg
-port tasks = app.tasks
 
 type Msg
-    = Header Move
+    = Grow
+    | Header Move
     | Shrink
-    | Grow
-    | Animate UI.Action
+    | Animate UI.Msg
 
 
 type alias Model =
-    { style : Animation msg }
+    { style : UI.State
+    }
 
 
-init : Model
+init : (Model, Cmd Msg)
 init = 
-  { style =
-      Animation.style
-        [ Animation.width 100 percent
-        , Animation.height 90 px
-        , Animation.backgroundColor 75 75 75 1
+  ({ style =
+      UI.style
+        [ UI.width (px 700)
+        , UI.height (px 90)
+        , UI.backgroundColor (rgba 75 75 75 1)
         ]
   }
+  , Cmd.none
+  )
+
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update message model =
     case message of
-        Animate message ->
-            onModel model message
-        Grow ->
-            UI.animate
-                |> UI.duration (2*second)
-                |> UI.props
-                    [ Height (UI.to 200) px ]
-                |> onModel model
-        Shrink ->
-            UI.animate
-                |> UI.props
-                    [ Height (UI.to 90) px ]
-                |> onModel model
-        Header move ->
-            Scroll.handle
-                [ update Grow
-                  |> Scroll.onCrossDown 400
-                , update Shrink
-                  |> Scroll.onCrossUp 400
+      Header move ->
+        Scroll.handle
+            [ update Grow
+              |> Scroll.onCrossDown 400
+            , update Shrink
+              |> Scroll.onCrossUp 400
+            ]
+            move model
+
+      Grow ->
+        let
+            newStyle =
+              UI.interrupt
+                [ UI.to
+                  [ UI.height (px 200) ]
                 ]
-                move model
+                model.style
+        in
+           ({ model | style = newStyle }, Cmd.none)
+
+      Shrink ->
+        let
+            newStyle =
+              UI.interrupt
+                [ UI.to
+                  [ UI.height (px 90) ]
+                ]
+                model.style
+        in
+           ({ model | style = newStyle }, Cmd.none)
+
+      Animate animMsg ->
+        ({ model | style = UI.update animMsg model.style }, Cmd.none)
     
-onModel : Model -> Msg -> (Model, Cmd Msg)
-onModel model message =
-  let
-      newStyle =
-        UI.interrupt
-          [ UI.to message ]
-          model.style
-
-  in
-     { model | style = newStyle }
-
+onModel : Model -> UI.Msg -> (Model, Cmd Msg)
+onModel model animMsg =
+  ({ model | style = UI.update animMsg model.style }, Cmd.none)
 
 view : Model -> Html Msg
-view message model =
-    div [] 
-        [ div [ style <| ("position", "fixed") :: UI.render model.style ]
-            []
-        , div [ style [("height", "10000px")] ] [] ]
+view model =
+  div []
+    [
+      div
+        (UI.render model.style
+                    ++ [ style
+                         [ ( "position", "fixed" )
+                         ]
+                       ]
+        ) []
+      , div
+          [ style
+            [ ("height", "3000px") ]
+          ]
+          []
+    ]
 
 
-port scroll : Move -> Sub msg
+port scroll : (Move -> msg) -> Sub msg
